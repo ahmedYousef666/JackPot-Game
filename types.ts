8d6ea9f6c11f5a2d70c4b2d48f0cf64cf5050872
ahmedYousef666 <ahmed-yousef@live.com>
2025-12-28 15:08:15 +0300

    Add files AAAsh

---
+++ types.ts
@@ -0,0 +1,12 @@
+
+export interface TextList {
+  id: string;
+  title: string;
+  items: string[];
+  rawText: string;
+}
+
+export interface SpinResult {
+  listId: string;
+  selectedItem: string;
+}
