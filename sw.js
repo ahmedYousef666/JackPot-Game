66ce33b505e7f661389b36b15afe5ec3f7f198a2
ahmedYousef666 <ahmed-yousef@live.com>
2025-12-30 09:24:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-
-const CACHE_NAME = 'slot-machine-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/index.tsx',
-  '/App.tsx',
-  '/types.ts'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('fetch', () => {});
